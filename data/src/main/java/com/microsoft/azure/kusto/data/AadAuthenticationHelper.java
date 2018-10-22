eabd0dee28800ffcd069b9e2a19e7cfddffe0f76
LizaShak <iliza@outlook.com>
2018-10-22 15:34:19 +0300

    authenticate with certificate only with objects

--- data/src/main/java/com/microsoft/azure/kusto/data/AadAuthenticationHelper.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/AadAuthenticationHelper.java
@@ -6,25 +6,10 @@ import com.microsoft.aad.adal4j.AuthenticationResult;
 import com.microsoft.aad.adal4j.ClientCredential;
 import com.microsoft.azure.kusto.data.exceptions.DataClientException;
 import com.microsoft.azure.kusto.data.exceptions.DataServiceException;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.cert.X509CertificateHolder;
-import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
-import org.bouncycastle.openssl.jcajce.JceOpenSSLPKCS8DecryptorProviderBuilder;
-import org.bouncycastle.operator.InputDecryptorProvider;
-import org.bouncycastle.operator.OperatorCreationException;
-import org.bouncycastle.pkcs.PKCS8EncryptedPrivateKeyInfo;
-import org.bouncycastle.pkcs.PKCSException;
 
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.security.PrivateKey;
-import java.security.Security;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -132,52 +117,4 @@ class AadAuthenticationHelper {
         return result;
     }
 
-    public AuthenticationResult acquireWithClientCertificate(String certPath, String keyPath, String pemPassword)
-            throws IOException, CertificateException, OperatorCreationException, PKCSException,
-            InterruptedException, ExecutionException{
-        final KeyCert certificateKey = readPem(certPath, pemPassword);
-
-        final PrivateKey privateKey = readPem(keyPath, pemPassword).getKey();
-
-        AuthenticationContext context;
-        context = new AuthenticationContext(aadAuthorityUri, false, Executors.newFixedThreadPool(1));
-        AsymmetricKeyCredential asymmetricKeyCredential = AsymmetricKeyCredential.create(clientCredential.getClientId(),
-                privateKey, certificateKey.getCertificate());
-        // pass null value for optional callback function and acquire access token
-        AuthenticationResult result = context.acquireToken(clusterUrl, asymmetricKeyCredential, null).get();
-
-        return result;
-    }
-
-    private static KeyCert readPem(String path, String password) throws IOException, CertificateException, OperatorCreationException, PKCSException {
-
-        Security.addProvider(new BouncyCastleProvider());
-        PEMParser pemParser = new PEMParser(new FileReader(new File(path)));
-        PrivateKey privateKey = null;
-        X509Certificate cert = null;
-        Object object = pemParser.readObject();
-
-        while (object != null) {
-            JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-            if (object instanceof X509CertificateHolder) {
-                cert = new JcaX509CertificateConverter().getCertificate((X509CertificateHolder) object);
-            }
-            if (object instanceof PKCS8EncryptedPrivateKeyInfo) {
-                PKCS8EncryptedPrivateKeyInfo pinfo = (PKCS8EncryptedPrivateKeyInfo) object;
-                InputDecryptorProvider provider = new JceOpenSSLPKCS8DecryptorProviderBuilder().build(password.toCharArray());
-                PrivateKeyInfo info = pinfo.decryptPrivateKeyInfo(provider);
-                privateKey = converter.getPrivateKey(info);
-            }
-            if (object instanceof PrivateKeyInfo) {
-                privateKey = converter.getPrivateKey((PrivateKeyInfo) object);
-            }
-            object = pemParser.readObject();
-        }
-
-        KeyCert keycert = new KeyCert(null, null);
-        keycert.setCertificate(cert);
-        keycert.setKey(privateKey);
-        pemParser.close();
-        return keycert;
-    }
 }
