11d7f749f88e269c68cb19d7efd1a9d16d794b8f
rabee333 <rabeea.abusaleh@gmail.com>
2018-11-04 17:44:21 +0200

    Add getter and setter og SourceId to SourceInfo interface

--- ingest/src/main/java/com/microsoft/azure/kusto/ingest/source/SourceInfo.java
+++ ingest/src/main/java/com/microsoft/azure/kusto/ingest/source/SourceInfo.java
@@ -1,5 +1,9 @@
 package com.microsoft.azure.kusto.ingest.source;
 
+import java.util.UUID;
+
 public interface SourceInfo {
     void validate();
+    UUID getSourceId();
+    void setSourceId(UUID sourceId);
 }
