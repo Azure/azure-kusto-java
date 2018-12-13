facdce5c2d85f6194ffdab2edda9e7bc94740482
Daniel Dubovski <dadubovs@microsoft.com>
2018-12-13 16:43:16 +0200

    Feature/client request properties (#57)
    * add `ClientRequestProperties`

---
+++ data/src/test/java/com/microsoft/azure/kusto/data/ClientRequestPropertiesTest.java
@@ -0,0 +1,34 @@
+package com.microsoft.azure.kusto.data;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import java.util.concurrent.TimeUnit;
+
+public class ClientRequestPropertiesTest {
+    @Test
+    @DisplayName("test set/get timeout")
+    void timeoutSetGet() {
+        ClientRequestProperties props = new ClientRequestProperties();
+        Long expected = TimeUnit.MINUTES.toMillis(100);
+
+        // before setting value should be null
+        Assertions.assertEquals(null, props.getTimeoutInMilliSec());
+
+        props.setTimeoutInMilliSec(expected);
+        Assertions.assertEquals(expected, props.getTimeoutInMilliSec());
+    }
+
+    @Test
+    @DisplayName("test ClientRequestProperties toString")
+    void propertiesToString() throws JSONException {
+        ClientRequestProperties props = new ClientRequestProperties();
+        props.setOption("a", 1);
+        props.setOption("b", "hello");
+
+        JSONAssert.assertEquals("{\"Options\": {\"a\":1, \"b\":\"hello\"}}", props.toString(), false);
+    }
+}
