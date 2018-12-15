5b218c73476daf1dfb47749ce90d2033255e109d
tamirkamara <26870601+tamirkamara@users.noreply.github.com>
2018-12-15 20:25:29 +0200

    ResultSetSourceInfo tests and comments

--- ingest/src/main/java/com/microsoft/azure/kusto/ingest/source/ResultSetSourceInfo.java
+++ ingest/src/main/java/com/microsoft/azure/kusto/ingest/source/ResultSetSourceInfo.java
@@ -4,23 +4,44 @@ import java.sql.ResultSet;
 import java.util.Objects;
 import java.util.UUID;
 
+/**
+ * Represents the Resultset source information used for ingestion.
+ */
 public class ResultSetSourceInfo extends AbstractSourceInfo {
 
     private ResultSet resultSet;
 
+    /**
+     * Creates a ResultSetSourceInfo.
+     *
+     * @param resultSet The ResultSet with the data to be ingested.
+     */
     public ResultSetSourceInfo(ResultSet resultSet) {
         setResultSet(resultSet);
     }
 
+    /**
+     * Creates a ResultSetSourceInfo
+     * @param resultSet The ResultSet with the data to be ingested.
+     * @param sourceId An identifier that could later be used to trace this specific source data.
+     */
     public ResultSetSourceInfo(ResultSet resultSet, UUID sourceId) {
         setResultSet(resultSet);
         this.setSourceId(sourceId);
     }
 
+    /**
+     * Gets the ResultSet.
+     * @return
+     */
     public ResultSet getResultSet() {
         return resultSet;
     }
 
+    /**
+     * Sets the ResultSet.
+     * @param resultSet The ResultSet with the data to be ingested.
+     */
     @SuppressWarnings("WeakerAccess")
     public void setResultSet(ResultSet resultSet) {
         this.resultSet = Objects.requireNonNull(resultSet, "ResultSet cannot be null");
@@ -31,6 +52,9 @@ public class ResultSetSourceInfo extends AbstractSourceInfo {
         return String.format("ResultSet with SourceId: %s", getSourceId());
     }
 
+    /**
+     * Checks that this SourceInfo is defined appropriately.
+     */
     public void validate() {
         //nothing to validate as of now.
     }
