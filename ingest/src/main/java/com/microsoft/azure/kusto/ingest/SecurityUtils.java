532c04429f64e2c6d5ce1e37288133a870a6155b
ohadbitt <32278684+ohadbitt@users.noreply.github.com>
2019-05-15 16:18:31 +0300

    Update ingest/src/main/java/com/microsoft/azure/kusto/ingest/SecurityUtils.java
    Co-Authored-By: Tomer Shetah <32904797+toshetah@users.noreply.github.com>

--- ingest/src/main/java/com/microsoft/azure/kusto/ingest/SecurityUtils.java
+++ ingest/src/main/java/com/microsoft/azure/kusto/ingest/SecurityUtils.java
@@ -2,6 +2,6 @@ package com.microsoft.azure.kusto.ingest;
 
 class SecurityUtils {
     static String removeSecretsFromUrl(String url) {
-        return url.split("[?]", 2)[0];
+        return url.split("[?]", 2)[0].split("[;]", 2)[0];
     }
 }
