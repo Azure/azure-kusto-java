0091f2342570c4fe5d48fbde4c8e020b59cf3533
Ohad Bitton <ohbitton@microsoft.com>
2020-03-08 10:27:41 +0200

    better

--- samples/src/main/java/Query.java
+++ samples/src/main/java/Query.java
@@ -8,32 +8,27 @@ import java.util.concurrent.TimeUnit;
 public class Query {
 
     public static void main(String[] args) {
-//
-//        try {
-//            ConnectionStringBuilder csb = ConnectionStringBuilder.createWithAadApplicationCredentials(
-//                    "https://ohbitton.dev.kusto.windows.net",
-//                    "d5e0a24c-3a09-40ce-a1d6-dc5ab58dae66",
-//                    "L+0hoM34kqC22XRniWOgkETwVvawiir2odEjYqZeyXA=");
-//            ClientImpl client = new ClientImpl(csb);
-//
-//            KustoResponseResults results = client.execute( ("ohtst"), ("TestTable2 | where ColA has 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'"));
-//            KustoResultSetTable mainTableResultSet = results.getPrimaryResults();
-//            mainTableResultSet
-//            while (mainTableResultSet.next())
-//            {
-//                System.out.println(mainTableResultSet.getCurrentRow().get(0));
-//                System.out.println(mainTableResultSet.getString(1));
-//            }
-//            System.out.println(String.format("Kusto sent back %s rows.", mainTableResultSet.count()));
-//
-//            // in case we want to pass client request properties
-//            ClientRequestProperties clientRequestProperties = new ClientRequestProperties();
-//            clientRequestProperties.setTimeoutInMilliSec(TimeUnit.MINUTES.toMillis(1));
-//
-//            results = client.execute( System.getProperty("dbName"), System.getProperty("query"), clientRequestProperties);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+
+        try {
+            ConnectionStringBuilder csb = ConnectionStringBuilder.createWithAadApplicationCredentials(
+                    System.getProperty("clusterPath"),
+                    System.getProperty("appId"),
+                    System.getProperty("appKey"),
+                    System.getProperty("appTenant"));
+            ClientImpl client = new ClientImpl(csb);
+
+            Results results = client.execute( System.getProperty("dbName"), System.getProperty("query"));
+
+            System.out.println(String.format("Kusto sent back %s rows.", results.getValues().size()));
+
+            // in case we want to pass client request properties
+            ClientRequestProperties clientRequestProperties = new ClientRequestProperties();
+            clientRequestProperties.setTimeoutInMilliSec(TimeUnit.MINUTES.toMillis(1));
+
+            results = client.execute( System.getProperty("dbName"), System.getProperty("query"), clientRequestProperties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
