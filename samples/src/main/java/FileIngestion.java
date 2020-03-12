1cc2786131dcbb5fb37a37d093d52e09cb020f1c
Ohad Bitton <ohbitton@microsoft.com>
2020-03-12 18:32:32 +0200

    revert

--- samples/src/main/java/FileIngestion.java
+++ samples/src/main/java/FileIngestion.java
@@ -34,12 +34,10 @@ public class FileIngestion {
             // Ingest with inline ingestion mapping - less recommended
             IngestionProperties ingestionProperties2 = new IngestionProperties(System.getProperty("dbName"),
                     System.getProperty("tableName"));
-            ColumnMapping csvColumnMapping = new ColumnMapping("ColA", "string", new HashMap<String,String>(){{
-                put(MappingConst.ORDINAL.name(), "0");
-            }});
-            ColumnMapping csvColumnMapping2 = new ColumnMapping("ColB", "int", new HashMap<String,String>(){{
-                put(MappingConst.ORDINAL.name(), "1");
-            }});
+            ColumnMapping csvColumnMapping = new ColumnMapping("ColA", "string");
+            csvColumnMapping.setCsvOrdinal(0);
+            ColumnMapping csvColumnMapping2 = new ColumnMapping("ColB", "int");
+            csvColumnMapping2.setCsvOrdinal(1);
             ingestionProperties2.setDataFormat("Csv");
             ingestionProperties2.setIngestionMapping(new ColumnMapping[]{csvColumnMapping, csvColumnMapping2}, IngestionMapping.IngestionMappingKind.Csv);
             IngestionResult ingestionResult2 = client.ingestFromStream(info, ingestionProperties2);
