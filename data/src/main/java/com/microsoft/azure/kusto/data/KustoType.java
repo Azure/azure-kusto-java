4a68d079f4810803997185e00b6b2d7cc929664d
maltalex <code@bit48.net>
2021-01-12 12:44:23 +0200

    KustoType formatting - don't align field declarations

--- data/src/main/java/com/microsoft/azure/kusto/data/KustoType.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/KustoType.java
@@ -16,25 +16,23 @@ import java.util.function.Function;
  */
 public class KustoType<C> {
 	
-	public static final KustoType<Integer>			INTEGER						= new KustoType<>(Integer.class);
+	public static final KustoType<Integer> INTEGER = new KustoType<>(Integer.class);
 	// Longs often return as "Integer", so we need to cast to "number", and then get the long value:
-	public static final KustoType<Long>				LONG						= new KustoType<>(Long.class,
-			o -> Long.valueOf(Number.class.cast(o).longValue()));
-	public static final KustoType<UUID>				GUID_UUID					= new KustoType<>(UUID.class, o -> UUID.fromString(o.toString()));
-	public static final KustoType<String>			GUID_STRING					= new KustoType<>(String.class);
-	public static final KustoType<String>			STRING						= new KustoType<>(String.class);
-	public static final KustoType<Double>			REAL_DOUBLE					= new KustoType<>(Double.class);
-	public static final KustoType<Float>			REAL_FLOAT					= new KustoType<>(Float.class,
-			o -> Float.valueOf(Double.class.cast(o).floatValue()));
+	public static final KustoType<Long> LONG = new KustoType<>(Long.class, o -> Long.valueOf(Number.class.cast(o).longValue()));
+	public static final KustoType<UUID> GUID_UUID = new KustoType<>(UUID.class, o -> UUID.fromString(o.toString()));
+	public static final KustoType<String> GUID_STRING = new KustoType<>(String.class);
+	public static final KustoType<String> STRING = new KustoType<>(String.class);
+	public static final KustoType<Double> REAL_DOUBLE = new KustoType<>(Double.class);
+	public static final KustoType<Float> REAL_FLOAT = new KustoType<>(Float.class, o -> Float.valueOf(Double.class.cast(o).floatValue()));
 	
-	public static final KustoType<ZonedDateTime>	DATETIME_ZONED_DATE_TIME	= new KustoType<>(ZonedDateTime.class, o -> ZonedDateTime.parse(o.toString()));
-	public static final KustoType<Instant>			DATETIME_INSTANT			= new KustoType<>(Instant.class, o -> Instant.parse(o.toString()));
-	public static final KustoType<Long>				DATETIME_LONG				= new KustoType<>(Long.class, o -> Instant.parse(o.toString()).toEpochMilli());
+	public static final KustoType<ZonedDateTime> DATETIME_ZONED_DATE_TIME = new KustoType<>(ZonedDateTime.class, o -> ZonedDateTime.parse(o.toString()));
+	public static final KustoType<Instant> DATETIME_INSTANT = new KustoType<>(Instant.class, o -> Instant.parse(o.toString()));
+	public static final KustoType<Long> DATETIME_LONG = new KustoType<>(Long.class, o -> Instant.parse(o.toString()).toEpochMilli());
 	
-	public static final KustoType<Object>			OBJECT						= new KustoType<>(Object.class);
+	public static final KustoType<Object> OBJECT = new KustoType<>(Object.class);
 	
-	final Class<C>									clazz;
-	final Function<Object, C>						typer;
+	final Class<C> clazz;
+	final Function<Object, C> typer;
 	
 	private KustoType(Class<C> clazz) {
 		this(clazz, clazz::cast);
