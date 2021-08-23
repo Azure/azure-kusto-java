d57522daa28354451c2aaab8fcd14836fbc19449
Yihezkel Schoenbrun <yischoen@microsoft.com>
2021-08-23 14:02:23 +0300

    Revert KustoDataExceptionBase to be abstract

--- data/src/main/java/com/microsoft/azure/kusto/data/exceptions/KustoDataExceptionBase.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/exceptions/KustoDataExceptionBase.java
@@ -1,10 +1,10 @@
 package com.microsoft.azure.kusto.data.exceptions;
 
-public class KustoDataExceptionBase extends Exception {
+public abstract class KustoDataExceptionBase extends Exception {
     private final String ingestionSource;
     private final boolean isPermanent;
 
-    public KustoDataExceptionBase(String ingestionSource, String message, Exception exception, boolean isPermanent) {
+    protected KustoDataExceptionBase(String ingestionSource, String message, Exception exception, boolean isPermanent) {
         super(message, exception);
         this.ingestionSource = ingestionSource;
         this.isPermanent = isPermanent;
