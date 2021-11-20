10ef69c9b49b278641193db834b48c0e15826e74
Ohad Bitton <ohbitton@microsoft.com>
2021-11-20 13:07:56 +0200

    tests revert

--- samples/src/main/java/FileIngestion.java
+++ samples/src/main/java/FileIngestion.java
@@ -4,7 +4,6 @@
 import com.microsoft.azure.kusto.data.auth.ConnectionStringBuilder;
 import com.microsoft.azure.kusto.ingest.*;
 import com.microsoft.azure.kusto.ingest.result.IngestionResult;
-import com.microsoft.azure.kusto.ingest.source.BlobSourceInfo;
 import com.microsoft.azure.kusto.ingest.source.FileSourceInfo;
 import com.microsoft.azure.kusto.ingest.source.StreamSourceInfo;
 
@@ -13,6 +12,38 @@ import java.io.ByteArrayOutputStream;
 
 public class FileIngestion {
     public static void main(String[] args) {
+        try {
+            ConnectionStringBuilder csb =
+                    ConnectionStringBuilder.createWithAadApplicationCredentials(System.getProperty("clusterPath"),
+                            System.getProperty("appId"),
+                            System.getProperty("appKey"),
+                            System.getProperty("appTenant"));
+            try (IngestClient client = IngestClientFactory.createClient(csb)) {
+                IngestionProperties ingestionProperties = new IngestionProperties(System.getProperty("dbName"),
+                        System.getProperty("tableName"));
+                ingestionProperties.setIngestionMapping(System.getProperty("dataMappingName"), IngestionMapping.IngestionMappingKind.Json);
 
+                FileSourceInfo fileSourceInfo = new FileSourceInfo(System.getProperty("filePath"), 0);
+                IngestionResult ingestionResult = client.ingestFromFile(fileSourceInfo, ingestionProperties);
+                ByteArrayOutputStream st = new ByteArrayOutputStream();
+                st.write("asd,2".getBytes());
+                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(st.toByteArray());
+                StreamSourceInfo info = new StreamSourceInfo(byteArrayInputStream);
+
+                // Ingest with inline ingestion mapping - less recommended
+                IngestionProperties ingestionProperties2 = new IngestionProperties(System.getProperty("dbName"),
+                        System.getProperty("tableName"));
+                ColumnMapping csvColumnMapping = new ColumnMapping("ColA", "string");
+                csvColumnMapping.setOrdinal(0);
+                ColumnMapping csvColumnMapping2 = new ColumnMapping("ColB", "int");
+                csvColumnMapping2.setOrdinal(1);
+                ingestionProperties2.setDataFormat(IngestionProperties.DataFormat.csv);
+                ingestionProperties2.setIngestionMapping(new ColumnMapping[]{csvColumnMapping, csvColumnMapping2}, IngestionMapping.IngestionMappingKind.Csv);
+
+                IngestionResult ingestionResult2 = client.ingestFromStream(info, ingestionProperties2);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
