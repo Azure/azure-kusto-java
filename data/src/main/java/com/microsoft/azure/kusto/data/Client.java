6f8c2f4eec836972d47d39273f76e6079c74da41
AsafMah <asafmahlev@microsoft.com>
2022-02-24 08:41:03 +0200

    Update Client.java

--- data/src/main/java/com/microsoft/azure/kusto/data/Client.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/Client.java
@@ -6,7 +6,7 @@ package com.microsoft.azure.kusto.data;
 import com.microsoft.azure.kusto.data.exceptions.DataClientException;
 import com.microsoft.azure.kusto.data.exceptions.DataServiceException;
 
-public interface Client {
+public interface Client2 {
     KustoOperationResult execute(String command) throws DataServiceException, DataClientException;
 
     KustoOperationResult execute(String database, String command) throws DataServiceException, DataClientException;
