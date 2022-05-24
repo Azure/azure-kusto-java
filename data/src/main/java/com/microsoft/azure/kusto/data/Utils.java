07b022e225dd6426b1781b5ecb1a878942a92b20
AsafMah <asafmahlev@microsoft.com>
2022-05-24 14:47:52 +0300

    Fixed missing exception

--- data/src/main/java/com/microsoft/azure/kusto/data/Utils.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/Utils.java
@@ -9,6 +9,7 @@ import com.microsoft.azure.kusto.data.exceptions.DataServiceException;
 import com.microsoft.azure.kusto.data.exceptions.DataWebException;
 import com.microsoft.azure.kusto.data.exceptions.OneApiError;
 import com.microsoft.azure.kusto.data.exceptions.WebException;
+import com.microsoft.azure.kusto.data.exceptions.ThrottleException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Header;
