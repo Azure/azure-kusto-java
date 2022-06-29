f7af546b70f9e1c37f67455d74d435d8cb7e8b77
Cole <The-Funk@users.noreply.github.com>
2022-06-29 15:36:13 -0400

    wrong httpclient import in TokenProviderBase

--- data/src/main/java/com/microsoft/azure/kusto/data/auth/TokenProviderBase.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/auth/TokenProviderBase.java
@@ -5,7 +5,7 @@ import com.microsoft.azure.kusto.data.exceptions.DataClientException;
 import com.microsoft.azure.kusto.data.exceptions.DataServiceException;
 import java.net.URISyntaxException;
 
-import org.apache.http.client.HttpClient;
+import org.apache.hc.client5.http.classic.HttpClient;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
