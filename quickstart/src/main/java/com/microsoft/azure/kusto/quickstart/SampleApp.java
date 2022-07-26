134703db9931160ce4c95e8402ec3a0344c1e23a
ronmoneta <t-ronmoneta@microsoft.com>
2022-07-26 12:58:15 +0300

    Utils

--- quickstart/src/main/java/com/microsoft/azure/kusto/quickstart/SampleApp.java
+++ quickstart/src/main/java/com/microsoft/azure/kusto/quickstart/SampleApp.java
@@ -1,4 +1,43 @@
 package com.microsoft.azure.kusto.quickstart;
 
+/**
+ * SourceType - represents the type of files used for ingestion
+ */
+enum SourceType {
+    localFileSource("Local File Source"),
+    blobSource("Blob Source");
+
+    private String source;
+
+
+    SourceType(String source) {
+        this.source = source;
+    }
+
+    public String getSource() {
+        return source;
+    }
+}
+
+/**
+ * AuthenticationModeOptions - represents the different options to autenticate to the system
+ */
+enum AuthenticationModeOptions {
+    userPrompt("UserPrompt"),
+    managedIdentity("ManagedIdentity"),
+    appKey("AppKey"),
+    appCertificate("AppCertificate");
+
+    private String mode;
+
+    AuthenticationModeOptions(String mode) {
+        this.mode = mode;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+}
+
 public class SampleApp {
 }
