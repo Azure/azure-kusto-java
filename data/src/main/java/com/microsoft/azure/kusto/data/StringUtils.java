a48bb62861d24e023ac31f2be1155f0ef91c6a14
Yihezkel Schoenbrun <yischoen@microsoft.com>
2022-08-31 15:39:21 +0300

    Support tables with special characters .-_ (#269)
    * Support tables with special characters .-_
    
    * Align normalizeEntityName with its C# equivalent

--- data/src/main/java/com/microsoft/azure/kusto/data/StringUtils.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/StringUtils.java
@@ -1,7 +1,11 @@
 package com.microsoft.azure.kusto.data;
 
-public class StringUtils {
-    public static String GetStringTail(String val, int minRuleLength) {
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+    private StringUtils() {
+        // Hide constructor for static class
+    }
+
+    public static String getStringTail(String val, int minRuleLength) {
         if (minRuleLength <= 0) {
             return "";
         }
@@ -13,6 +17,15 @@ public class StringUtils {
         return val.substring(val.length() - minRuleLength);
     }
 
-    private StringUtils() {
+    public static String normalizeEntityName(String name) {
+        if (StringUtils.isBlank(name)) {
+            return name;
+        } else if (name.startsWith("[")) {
+            return name;
+        } else if (!name.contains("'")) {
+            return "['" + name + "']";
+        } else {
+            return "[\"" + name + "\"]";
+        }
     }
 }
