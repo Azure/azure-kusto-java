835bb0232a6203d411cf613b385a66ee89c91589
t-ronmoneta <t-ronmoneta@microsoft.com>
2022-11-03 15:04:53 +0200

    fixed maven

--- quickstart/src/main/java/com/microsoft/azure/kusto/quickstart/SampleApp.java
+++ quickstart/src/main/java/com/microsoft/azure/kusto/quickstart/SampleApp.java
@@ -522,7 +522,8 @@ public class SampleApp {
 
         // Tip: Kusto's Java SDK can ingest data from files, blobs, java.sql.ResultSet objects, and open streams.
         // See the SDK's kusto-samples module and the E2E tests in kusto-ingest for additional references.
-        // Note: No need to add "nosource" option as in that case the "ingestData" flag will be set to false, and it will be imppossible to reach this code segemnt.
+        // Note: No need to add "nosource" option as in that case the "ingestData" flag will be set to false, and it will be imppossible to reach this code
+        // segemnt.
         switch (sourceType) {
             case LOCAL_FILE_SOURCE:
                 Utils.Ingestion.ingestFromFile(ingestClient, databaseName, tableName, uri, dataFormat, mappingName, ignoreFirstRecord);
