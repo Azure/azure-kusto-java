0e50ebf194f92165dd2b44768601f6cdeb14c58d
AsafMah <asafmahlev@microsoft.com>
2023-02-08 08:42:23 +0200

    public

--- data/src/main/java/com/microsoft/azure/kusto/data/ClientImpl.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/ClientImpl.java
@@ -37,9 +37,9 @@ class ClientImpl implements Client, StreamingClient {
     private static final Long QUERY_TIMEOUT_IN_MILLISECS = TimeUnit.MINUTES.toMillis(4);
     private static final Long STREAMING_INGEST_TIMEOUT_IN_MILLISECS = TimeUnit.MINUTES.toMillis(10);
     private static final int CLIENT_SERVER_DELTA_IN_MILLISECS = (int) TimeUnit.SECONDS.toMillis(30);
-    private static final String CLIENT_VERSION_HEADER = "x-ms-client-version";
-    private static final String APP_HEADER = "x-ms-app";
-    private static final String USER_HEADER = "x-ms-user";
+    public static final String CLIENT_VERSION_HEADER = "x-ms-client-version";
+    public static final String APP_HEADER = "x-ms-app";
+    public static final String USER_HEADER = "x-ms-user";
     public static final String FEDERATED_SECURITY_SUFFIX = ";fed=true";
     public static final String JAVA_INGEST_ACTIVITY_TYPE_PREFIX = "DN.JavaClient.Execute";
     private final TokenProviderBase aadAuthenticationHelper;
