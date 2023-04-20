77fcabc808c91f53426356018d8bfbc8436f00a3
enmoed <101040590+enmoed@users.noreply.github.com>
2023-04-20 17:10:50 +0300

    tests

--- data/src/main/java/com/microsoft/azure/kusto/data/instrumentation/DistributedTracing.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/instrumentation/DistributedTracing.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
-import java.util.HashMap;
 import java.util.Map;
 
 public class DistributedTracing {
