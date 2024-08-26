6355bb8ab7d3acc245a1de49eeb373e43785cfc3
Ohad Bitton <ohbitton@microsoft.com>
2024-08-26 16:01:29 +0300

    move SimpleProxyPlanner to test

--- samples/src/main/java/Proxy.java
+++ samples/src/main/java/Proxy.java
@@ -2,12 +2,17 @@ import com.microsoft.azure.kusto.data.Client;
 import com.microsoft.azure.kusto.data.ClientFactory;
 import com.microsoft.azure.kusto.data.HttpClientProperties;
 import com.microsoft.azure.kusto.data.auth.ConnectionStringBuilder;
-import com.microsoft.azure.kusto.data.http.SimpleProxyPlanner;
+import org.apache.http.HttpHost;
+import org.apache.http.conn.routing.HttpRoute;
 
 public class Proxy {
     public static void main(String[] args) {
+        // See file test\java\com\microsoft\azure\kusto\data\http\SimpleProxyPlanner for an example of a custom route planner class
         HttpClientProperties providedProperties = HttpClientProperties.builder()
-                .routePlanner(new SimpleProxyPlanner("localhost", 8080, "http", ".*kusto.windows.*")).build();
+                .routePlanner((host, request, context) -> {
+                    // Your custom route planning logic here
+                    return new HttpRoute(new HttpHost("example.com"));
+                }).build();
         try (Client client = ClientFactory.createClient(ConnectionStringBuilder.createWithUserPrompt("https://ohadev.swedencentral.dev.kusto.windows.net"),
                 providedProperties)) {
             client.execute(".show version");
