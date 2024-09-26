ee8bb0cbb89b8bee6c7caea403d10ce8a83e35ef
cole <cole.snyder@gmork.tech>
2024-09-26 18:55:23 -0400

    close clients in E2ETest

--- ingest/src/test/java/com/microsoft/azure/kusto/ingest/E2ETest.java
+++ ingest/src/test/java/com/microsoft/azure/kusto/ingest/E2ETest.java
@@ -143,6 +143,8 @@ class E2ETest {
             Assertions.assertNotNull(tableName, "Table name was not set");
             Assertions.assertNotNull(DB_NAME, "DB name was not set");
             queryClient.executeToJsonResult(DB_NAME, String.format(".drop table %s ifexists", tableName));
+            ingestClient.close();
+            managedStreamingIngestClient.close();
         } catch (Exception ex) {
             Assertions.fail("Failed to drop table", ex);
         }
