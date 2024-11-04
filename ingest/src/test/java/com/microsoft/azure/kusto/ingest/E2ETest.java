f592ccaaf2d8e10885b499ccb2d6e59de8ecd865
asafmahlev <asafmahlev@microsoft.com>
2024-11-04 11:14:20 +0200

    Fix test

--- ingest/src/test/java/com/microsoft/azure/kusto/ingest/E2ETest.java
+++ ingest/src/test/java/com/microsoft/azure/kusto/ingest/E2ETest.java
@@ -144,7 +144,8 @@ class E2ETest {
     @AfterAll
     public static void tearDown() {
         try {
-            new ExponentialRetry<DataServiceException, DataClientException>(2).execute(attempt -> queryClient.execute(DB_NAME, String.format(".drop table %s ifexists skip-seal", tableName)));
+            new ExponentialRetry<DataServiceException, DataClientException>(2)
+                    .execute(attempt -> queryClient.executeMgmt(DB_NAME, String.format(".drop table %s ifexists skip-seal", tableName)));
             ingestClient.close();
             managedStreamingIngestClient.close();
         } catch (Exception ex) {
