2c6a15621f8856477b2dd30e5d71c1d0130e0efa
George Banasios <129333863+georgebanasios@users.noreply.github.com>
2025-01-06 18:35:51 +0200

    Fixes exception wrapping (#406)
    * fixes exception wrapping
    
    * removes ioexception handling

--- data/src/main/java/com/microsoft/azure/kusto/data/ClientImpl.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/ClientImpl.java
@@ -175,7 +175,13 @@ class ClientImpl extends BaseClient {
                     return new KustoOperationResult(jsonResult.getResult(), jsonResult.getEndpoint().endsWith("v2/rest/query") ? "v2" : "v1");
                 })
                 .onErrorMap(KustoServiceQueryError.class, e -> new DataServiceException(clusterEndpoint, e.getMessage(), e, e.isPermanent()))
-                .onErrorMap(Exception.class, e -> new DataClientException(clusterEndpoint, ExceptionUtils.getMessageEx(e), e));
+                .onErrorMap(Exception.class, e -> {
+                    if (e instanceof DataServiceException) {
+                        return e;
+                    }
+
+                    return new DataClientException(clusterEndpoint, ExceptionUtils.getMessageEx(e), e);
+                });
     }
 
     private Mono<String> executeWithTimeout(KustoRequest request, String nameOfSpan) {
@@ -312,7 +318,6 @@ class ClientImpl extends BaseClient {
                         .publishOn(Schedulers.boundedElastic())
                         .map(response -> new KustoOperationResult(response, "v1"))
                         .onErrorMap(KustoServiceQueryError.class, e -> new DataClientException(clusterEndpoint, e.getMessage(), e))
-                        .onErrorMap(IOException.class, e -> new DataClientException(clusterUrl, e.getMessage(), e))
                         .doFinally(signalType -> {
                             if (isStreamSource && !leaveOpen) {
                                 try {
