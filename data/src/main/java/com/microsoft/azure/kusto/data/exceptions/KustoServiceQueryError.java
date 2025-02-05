5e2155b3fa1c3691f30d84ca75193d5c0ec07f66
Asaf Mahlev <asafmahlev@microsoft.com>
2025-02-05 10:54:33 +0200

    Refactor error handling in Kusto API to streamline exception processing

--- data/src/main/java/com/microsoft/azure/kusto/data/exceptions/KustoServiceQueryError.java
+++ data/src/main/java/com/microsoft/azure/kusto/data/exceptions/KustoServiceQueryError.java
@@ -13,24 +13,44 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
   This class represents an error that returned from the query result
  */
 public class KustoServiceQueryError extends AzureException {
+    static final String EXCEPTIONS_MESSAGE = "Query execution failed with multiple inner exceptions:\n";
+
     private final List<RuntimeException> exceptions;
 
-    public KustoServiceQueryError(ArrayNode jsonExceptions, boolean isOneApi, String message) {
+    public KustoServiceQueryError(String message, List<RuntimeException> exceptions) {
         super(message);
-        this.exceptions = new ArrayList<>();
-        for (int j = 0; jsonExceptions != null && j < jsonExceptions.size(); j++) {
-            if (isOneApi) {
-                this.exceptions.add(new DataWebException(jsonExceptions.get(j).toString()));
-            } else {
-                this.exceptions.add(new RuntimeException(jsonExceptions.get(j).toString()));
-            }
-        }
+        this.exceptions = exceptions;
     }
 
     public KustoServiceQueryError(String message) {
         super(message);
         this.exceptions = new ArrayList<>();
         this.exceptions.add(new RuntimeException(message));
+    }
+
+    public static KustoServiceQueryError fromOneApiErrorArray(ArrayNode jsonExceptions, boolean isOneApi) {
+        List<RuntimeException> exceptions = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        if (jsonExceptions == null || jsonExceptions.isEmpty()) {
+            return new KustoServiceQueryError("No exceptions were returned from the service.");
+        }
+
+        if (jsonExceptions.size() > 1) {
+            sb.append(EXCEPTIONS_MESSAGE);
+        }
+
+        for (int i = 0; i < jsonExceptions.size(); i++) {
+            if (isOneApi) {
+                exceptions.add(new DataWebException(jsonExceptions.get(i).toString()));
+            } else {
+                exceptions.add(new RuntimeException(jsonExceptions.get(i).toString()));
+            }
+            sb.append(jsonExceptions.get(i).toString());
+            sb.append("\n");
+        }
+
+        return new KustoServiceQueryError(sb.toString(), exceptions);
     }
 
     public List<RuntimeException> getExceptions() {
