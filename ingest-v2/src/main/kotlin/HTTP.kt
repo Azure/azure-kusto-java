9836327d8bb2c07f714380915f4d3f77bd2cc23b
ag-ramachandran <ramacg@microsoft.com>
2025-09-02 19:05:30 +0530

    * Add boilerplate

---
+++ ingest-v2/src/main/kotlin/HTTP.kt
@@ -0,0 +1,20 @@
+package com.microsoft.azure.kusto
+
+import com.asyncapi.kotlinasyncapi.context.service.AsyncApiExtension
+import com.asyncapi.kotlinasyncapi.ktor.AsyncApiPlugin
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureHTTP() {
+    install(AsyncApiPlugin) {
+        extension = AsyncApiExtension.builder {
+            info {
+                title("Sample API")
+                version("1.0.0")
+            }
+        }
+    }
+}
