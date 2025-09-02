ac988fff01a9371d346a4a28e0112f8ac854ffc3
ag-ramachandran <ramacg@microsoft.com>
2025-09-02 19:05:30 +0530

    * Add boilerplate

---
+++ ingest-v2/src/main/kotlin/Serialization.kt
@@ -0,0 +1,20 @@
+package com.microsoft.azure.kusto
+
+import com.asyncapi.kotlinasyncapi.context.service.AsyncApiExtension
+import com.asyncapi.kotlinasyncapi.ktor.AsyncApiPlugin
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+    routing {
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
