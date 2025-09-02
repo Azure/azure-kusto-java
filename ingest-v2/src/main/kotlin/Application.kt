2ad23f408109c4fb79f0e1c9823bf226261d3f9b
ag-ramachandran <ramacg@microsoft.com>
2025-09-02 19:05:30 +0530

    * Add boilerplate

---
+++ ingest-v2/src/main/kotlin/Application.kt
@@ -0,0 +1,13 @@
+package com.microsoft.azure.kusto
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureHTTP()
+    configureRouting()
+}
