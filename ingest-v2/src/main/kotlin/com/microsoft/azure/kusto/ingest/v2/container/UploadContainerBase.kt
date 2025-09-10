8710f02307dcbc0c3ab0fe0b465a75cf0f6e8c2e
ramacg <ramacg@microsoft.com>
2025-09-10 12:15:08 +0000

    *Minor edits

--- ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/container/UploadContainerBase.kt
+++ ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/container/UploadContainerBase.kt
@@ -4,6 +4,6 @@ package com.microsoft.azure.kusto.ingest.v2.container
 
 import java.io.InputStream
 
-interface UploadContainerBase : ContainerBase {
+interface UploadContainerBase  {
     suspend fun uploadAsync(name: String, stream: InputStream): String
 }
