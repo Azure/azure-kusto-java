412d61338a01a5fcf9d7962ac09492123b999dda
Ramachandran A G <ramacg@microsoft.com>
2025-09-27 17:52:21 +0530

    * Update POM

--- ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/KustoBaseApiClient.kt
+++ ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/KustoBaseApiClient.kt
@@ -18,6 +18,7 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
+import org.slf4j.LoggerFactory
 import java.time.OffsetDateTime
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -27,7 +28,7 @@ open class KustoBaseApiClient(
     open val tokenCredential: TokenCredential,
     open val skipSecurityChecks: Boolean = false,
 ) {
-
+    private val logger = LoggerFactory.getLogger(KustoBaseApiClient::class.java)
     protected val setupConfig: (HttpClientConfig<*>) -> Unit = { config ->
         getClientConfig(config)
     }
