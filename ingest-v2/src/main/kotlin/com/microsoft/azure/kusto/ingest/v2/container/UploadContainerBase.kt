e6a89a32252ed028326b69473906128045f70539
Ramachandran A G <ramacg@microsoft.com>
2025-10-30 16:35:49 +0530

    * Remove unused classes

--- ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/container/UploadContainerBase.kt
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-package com.microsoft.azure.kusto.ingest.v2.container
-
-import java.io.InputStream
-
-interface UploadContainerBase {
-    suspend fun uploadAsync(name: String, stream: InputStream): String
-}
