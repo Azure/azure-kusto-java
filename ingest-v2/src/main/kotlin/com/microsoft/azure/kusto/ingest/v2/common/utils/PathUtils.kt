2b74d1c010e6c977d3559725834092d5db1da0b4
Ramachandran A G <ramacg@microsoft.com>
2025-10-30 16:34:27 +0530

    * Remove unused classes * Address review comments

--- ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/common/utils/PathUtils.kt
+++ ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/common/utils/PathUtils.kt
@@ -73,8 +73,8 @@ object PathUtils {
             }
         if (uriObj == null || !uriObj.isAbsolute) {
             // Not a valid absolute URI, treat as path
-            return uri.substringAfterLast('/', uri)
-                .substringAfterLast('\\', uri)
+            return uri.substringAfterLast('/')
+                .substringAfterLast("\\")
         }
         // For web URIs, extract last segment of the path, remove query/fragment
         val path = uriObj.path ?: ""
