bbb9ea41354ec2c5946e36d5803b110c978f572b
Tanmaya Panda <tanmayapanda@microsoft.com>
2025-12-24 01:41:39 +0530

    normalized url

--- ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/builders/ManagedStreamingIngestClientBuilder.kt
+++ ingest-v2/src/main/kotlin/com/microsoft/azure/kusto/ingest/v2/builders/ManagedStreamingIngestClientBuilder.kt
@@ -20,7 +20,7 @@ private constructor(private val dmUrl: String) :
         @JvmStatic
         fun create(dmUrl: String): ManagedStreamingIngestClientBuilder {
             require(dmUrl.isNotBlank()) { "Data Ingestion URI cannot be blank" }
-            return ManagedStreamingIngestClientBuilder(dmUrl)
+            return ManagedStreamingIngestClientBuilder(normalizeAndCheckDmUrl(dmUrl))
         }
     }
 
